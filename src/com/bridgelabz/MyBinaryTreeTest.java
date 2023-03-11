4d1da109d4c4279e2739be787443f6f021f27e27
debabrata-97 <debabrataszr@gmail.com>
2023-03-12 00:18:27 +0530

    Welcome to Binary Search Tree Program

---
+++ src/com/bridgelabz/MyBinaryTreeTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+public class MyBinaryTreeTest {
+    public static void main(String[] args) {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getsize();
+        System.out.println("Total num of Nodes = " + size);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        System.out.println("Now Total nodes = " + myBinaryTree.getsize());
+        myBinaryTree.search(63);
+    }
+}
